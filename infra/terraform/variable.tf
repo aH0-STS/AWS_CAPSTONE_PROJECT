1b481ab269a756411a64895ed4720aff71443f40
yaswanth <119861381+aH0-STS@users.noreply.github.com>
2025-06-24 15:10:46 +0530

    Update variable.tf

--- infra/terraform/variable.tf
+++ infra/terraform/variable.tf
@@ -3,26 +3,25 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
- 
+
 variable "db_subnet_group_name" {
   default     = "my-db-subnet-group"
   description = "Name for the DB Subnet Group"
 }
- 
+
 variable "db_username" {
   default     = "admin"
   description = "Root username for the MySQL Database"
 }
- 
+
 variable "db_password" {
   description = "Root password for the MySQL Database"
   type        = string
   sensitive   = true
-  default     = "kireeti123"
+  default     = "adminadmin"
 }
- 
+
 variable "azs" {
   type    = list(string)
   default = ["us-west-2a", "us-west-2b"]
 }
- 
